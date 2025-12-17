2db37d0b20b53b642b62e63c084d2afb407fe9fc
panda1189 <leopardhong1189@gmail.com>
2025-12-17 11:35:07 +0900

    test

---
+++ src/tower/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'tower'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='gossi',
+    maintainer_email='mincheol710313@gmail.com',
+    description='TODO: Package description',
+    license='BSD',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+                'cube_tower = tower.tower_test:main',
+        ],
+    },
+)
